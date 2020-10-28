4c3aa57d6c07a77d51032f7bcd1712657905e218
lautarolaghezza <lautariver35@gmail.com>
2020-10-27 21:56:34 -0300

    Initial commit

---
+++ src/main/java/com/challenge/java/spring/wolox/WoloxApplication.java
@@ -0,0 +1,13 @@
+package com.challenge.java.spring.wolox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WoloxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WoloxApplication.class, args);
+    }
+
+}
